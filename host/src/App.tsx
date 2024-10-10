02de473fbe54b9ea4cb3d4d66908c223489c93b8
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-10-11 00:15:30 +0800

    import remote component to host app

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Counter from 'remote/Counter';
 
 import './index.scss'
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <div>Name: host</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
+    <Counter/> 
   </div>
 )
 const rootElement = document.getElementById('app')
